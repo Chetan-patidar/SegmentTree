87a5ad574c2da7b06109abc310568b68fd995df3
CHETAN PATIDAR <54496343+Chetan-patidar@users.noreply.github.com>
2023-11-04 00:49:40 +0530

    Update medianet2.cpp

--- medianet2.cpp
+++ medianet2.cpp
@@ -1,31 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
-class dt{
-    public:
-    int data;
-    int setbit;
-    dt(char d){
-        if(d=='0'){
-            data = 0;
-            setbit = 0;
-        }
-        else{
-            data = 1;
-            setbit = 1;
-        }
-    }
-    dt(){
-        data=0;
-        setbit=0;
-    }
-};
-
 class SGT{
     public:
-    vector<dt> arr;
+    vector<pair<int,int>> arr;
     vector<int> lazy;
+    
     SGT(int n){
         arr.resize(4*n+1);
         lazy.resize(4*n+1,0);
@@ -33,76 +22,119 @@ class SGT{
     
     void build(int index,int low,int high,string str){
         if(low==high){
-            dt temp(int(str[low]));
-            //cout<<str[low]<<"  ";
-            //cout<<temp.setbit<<" ";
-            arr[index] = temp;
+            if(str[low]=='0'){
+                arr[index].first = 0;
+                arr[index].second = 0;
+            }
+            else{
+                arr[index].first = 1;
+                arr[index].second = 1;
+            }
             return;
         }
         
         int mid = (low+high)/2;
+        
         build(2*index+1,low,mid,str);
         build(2*index+2,mid+1,high,str);
-        dt t(0);
-        t.setbit = arr[2*index+1].setbit + arr[2*index+2].setbit;
-        arr[index] = t;
+        arr[index].first = 0;
+        arr[index].second = arr[2*index+1].second + arr[2*index+2].second;
+        return;
     }
     
     
     void update(int index,int low,int high,int l,int h){
         if(lazy[index]!=0){
-          if(lazy[index]%2==1){
-            int count = high-low+1;
-            seg[index].setbit = count - seg[index].setbit;
-            //propagate
-            if(low!=high){
-                lazy[2*index+1]++;
-                lazy[2*index+2]++;
-            }
-            lazy[index] = 0;
-          }
-          else{
-              lazy[index] = 0;
-          }
-        }
-      
-        if(high<l || r<low){
-          return;
-        }
-
-      
-        if(low==high){
-            if(arr[index].data=='0'){
-                arr[index].data = 1;
-                arr[index].setbit = 1;
+            int c = lazy[index];
+            if(lazy[index]%2==1){
+                arr[index].second = (high-low+1) - arr[index].second;
+                lazy[2*index+1] = c;
+                lazy[2*index+2] = c;
+                lazy[index] = 0;
             }
             else{
-                arr[index].data = 0;
-                arr[index].setbit = 0;
+                lazy[index] = 0;
             }
-            return;
         }
         
-        else if(l<=low && high<=r){
-            r
+        
+        if(high<l || h<low)
+            return;
+            
+        else if(l<=low && high<=h){
+            arr[index].second = (high-low+1) - arr[index].second;
+            lazy[2*index+1]++;
+            lazy[2*index+2]++;
         }
+        
+        else{
+            int mid = (low+high)/2;
+            
+            update(2*index+1,low,mid,l,h);
+            update(2*index+2,mid+1,high,l,h);
+            arr[index].second = arr[2*index+1].second + arr[2*index+2].second;
+        }
+        
+        return;
     }
     
-    
-    void print(int n){
-        for(int i=0;i<4*n;i++){
-            cout<<arr[i].setbit<<"   ";
+    int query(int index,int low,int high,int l,int h){
+        if(lazy[index]!=0){
+            int c = lazy[index];
+            if(lazy[index]%2==1){
+                arr[index].second = (high-low+1) - arr[index].second;
+                lazy[2*index+1] = c;
+                lazy[2*index+2] = c;
+                lazy[index] = 0;
+            }
+            else{
+                lazy[index] = 0;
+            }
         }
+        
+        if(high<l || h<low)
+            return 0;
+            
+        else if(l<=low && high<=h){
+            return arr[index].second;
+        }
+        else{
+            int mid = (low+high)/2;
+            int left = query(2*index+1,low,mid,l,h);
+            int right = query(2*index+2,mid+1,high,l,h);
+            return left+right;
+        }
+        return 0;
     }
 };
 
 int main()
 {
     //cout<<"Hello World";
+    vector<int> ans;
     string test = "10101";
     int n = test.size();
     SGT st(n);
     st.build(0,0,n-1,test);
-    st.print(n);
+    int q;
+    cin>>q;
+    while(q--){
+        int op;
+        cin>>op;
+        int l,r;
+        cin>>l>>r;
+        if(op==1){
+            st.update(0,0,n-1,l,r);
+        }
+        else{
+            int answer = st.query(0,0,n-1,l,r);
+            ans.push_back(answer);
+        }
+    }
+    
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<"    ";
+    }
+
     return 0;
 }
